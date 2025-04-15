6b94833f94eaeca8ae9c0eeb514867ebe2d0236b
Martin <martin_a_troncoso@hotmail.com>
2025-04-15 13:50:51 -0300

    Modifications on settings.py

--- Personal_project/settings.py
+++ Personal_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Application.apps.ApplicationConfig'
 ]
 
 MIDDLEWARE = [
@@ -119,8 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_FILES_DIRS = [
+    os.path.join(BASE_DIR, 'Application/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
