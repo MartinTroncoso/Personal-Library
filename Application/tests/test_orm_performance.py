70ba4699fa5bc2439ed40f47eb7a5023c0e9e651
Martin <martin_a_troncoso@hotmail.com>
2026-01-20 14:03:49 -0300

    Adding ORM test, using annotate and Count()

--- Application/tests/test_orm_performance.py
+++ Application/tests/test_orm_performance.py
@@ -1,6 +1,9 @@
+# flake8: noqa
+
 import pytest
 from django.db import connection
-from Application.models import Libro
+from django.db.models import Count
+from Application.models import Libro, Usuario
 from Application.tests.factories.book_factory import BookFactory
 from Application.tests.factories.user_factory import UserFactory
 
@@ -12,7 +15,7 @@ def test_select_related_reduces_queries() -> None:
 
     # Without optimization
     with connection.cursor():
-        # We take how many queries had been executed first
+        # We retrieve how many queries had been executed first
         # connection.queries only works when 'DEBUG = True'
         initial_queries = len(connection.queries)
 
@@ -33,3 +36,13 @@ def test_select_related_reduces_queries() -> None:
         queries_with = len(connection.queries) - initial_queries
 
     assert queries_with >= queries_without
+
+
+@pytest.mark.django_db
+def test_usuario_total_libros() -> None:
+    user = UserFactory()
+    BookFactory.create_batch(3, usuario=user)
+
+    usuario = Usuario.objects.annotate(total_libros=Count("libros")).get(id=user.id)  # type: ignore
+
+    assert usuario.total_libros == 3
